4a30c272db5e092ee1ff067108a73dfbad241732
Maurits Meester <maurits@e-mmer.nl>
2020-05-18 20:15:11 +0200

    new build with latest version

--- dist/vue-rellax.esm.js
+++ dist/vue-rellax.esm.js
@@ -7,7 +7,7 @@
  * Released under the MIT license
  * https://github.com/yutahaga/vue-rellax/blob/master/LICENSE
  */
-var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 function createCommonjsModule(fn, module) {
 	return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -34,7 +34,7 @@ var rellax = createCommonjsModule(function (module) {
     // Browser globals (root is window)
     root.Rellax = factory();
   }
-}(commonjsGlobal, function () {
+}(typeof window !== "undefined" ? window : commonjsGlobal, function () {
   var Rellax = function(el, options){
 
     var self = Object.create(Rellax.prototype);
@@ -58,6 +58,18 @@ var rellax = createCommonjsModule(function (module) {
     // store the id for later use
     var loopId = null;
 
+    // Test via a getter in the options object to see if the passive property is accessed
+    var supportsPassive = false;
+    try {
+      var opts = Object.defineProperty({}, 'passive', {
+        get: function() {
+          supportsPassive = true;
+        }
+      });
+      window.addEventListener("testPassive", null, opts);
+      window.removeEventListener("testPassive", null, opts);
+    } catch (e) {}
+
     // check what cancelAnimation method to use
     var clearLoop = window.cancelAnimationFrame || window.mozCancelAnimationFrame || clearTimeout;
 
@@ -78,12 +90,17 @@ var rellax = createCommonjsModule(function (module) {
     // Default Settings
     self.options = {
       speed: -2,
+	    verticalSpeed: null,
+	    horizontalSpeed: null,
+      breakpoints: [576, 768, 1201],
       center: false,
       wrapper: null,
       relativeToWrapper: false,
       round: true,
       vertical: true,
       horizontal: false,
+      verticalScrollAxis: "y",
+      horizontalScrollAxis: "x",
       callback: function() {},
     };
 
@@ -92,6 +109,29 @@ var rellax = createCommonjsModule(function (module) {
       Object.keys(options).forEach(function(key){
         self.options[key] = options[key];
       });
+    }
+
+    function validateCustomBreakpoints () {
+      if (self.options.breakpoints.length === 3 && Array.isArray(self.options.breakpoints)) {
+        var isAscending = true;
+        var isNumerical = true;
+        var lastVal;
+        self.options.breakpoints.forEach(function (i) {
+          if (typeof i !== 'number') isNumerical = false;
+          if (lastVal !== null) {
+            if (i < lastVal) isAscending = false;
+          }
+          lastVal = i;
+        });
+        if (isAscending && isNumerical) return;
+      }
+      // revert defaults if set incorrectly
+      self.options.breakpoints = [576, 768, 1201];
+      console.warn("Rellax: You must pass an array of 3 numbers in ascending order to the breakpoints option. Defaults reverted");
+    }
+
+    if (options && options.breakpoints) {
+      validateCustomBreakpoints();
     }
 
     // By default, rellax class
@@ -109,7 +149,8 @@ var rellax = createCommonjsModule(function (module) {
 
     // The elements don't exist
     else {
-      throw new Error("The elements you're trying to select don't exist.");
+      console.warn("Rellax: The elements you're trying to select don't exist.");
+      return;
     }
 
     // Has a wrapper and it exists
@@ -120,11 +161,23 @@ var rellax = createCommonjsModule(function (module) {
         if (wrapper) {
           self.options.wrapper = wrapper;
         } else {
-          throw new Error("The wrapper you're trying to use don't exist.");
+          console.warn("Rellax: The wrapper you're trying to use doesn't exist.");
+          return;
         }
       }
     }
 
+    // set a placeholder for the current breakpoint
+    var currentBreakpoint;
+
+    // helper to determine current breakpoint
+    var getCurrentBreakpoint = function (w) {
+      var bp = self.options.breakpoints;
+      if (w < bp[0]) return 'xs';
+      if (w >= bp[0] && w < bp[1]) return 'sm';
+      if (w >= bp[1] && w < bp[2]) return 'md';
+      return 'lg';
+    };
 
     // Get and cache initial position of all elements
     var cacheBlocks = function() {
@@ -146,16 +199,21 @@ var rellax = createCommonjsModule(function (module) {
 
       screenY = window.innerHeight;
       screenX = window.innerWidth;
+      currentBreakpoint = getCurrentBreakpoint(screenX);
+
       setPosition();
 
       cacheBlocks();
+
+      animate();
 
       // If paused, unpause and set listener for window resizing events
       if (pause) {
         window.addEventListener('resize', init);
         pause = false;
+        // Start the loop
+        update();
       }
-      animate();
     };
 
     // We want to cache the parallax blocks'
@@ -164,7 +222,34 @@ var rellax = createCommonjsModule(function (module) {
     var createBlock = function(el) {
       var dataPercentage = el.getAttribute( 'data-rellax-percentage' );
       var dataSpeed = el.getAttribute( 'data-rellax-speed' );
+      var dataXsSpeed = el.getAttribute( 'data-rellax-xs-speed' );
+      var dataMobileSpeed = el.getAttribute( 'data-rellax-mobile-speed' );
+      var dataTabletSpeed = el.getAttribute( 'data-rellax-tablet-speed' );
+      var dataDesktopSpeed = el.getAttribute( 'data-rellax-desktop-speed' );
+      var dataVerticalSpeed = el.getAttribute('data-rellax-vertical-speed');
+      var dataHorizontalSpeed = el.getAttribute('data-rellax-horizontal-speed');
+      var dataVericalScrollAxis = el.getAttribute('data-rellax-vertical-scroll-axis');
+      var dataHorizontalScrollAxis = el.getAttribute('data-rellax-horizontal-scroll-axis');
       var dataZindex = el.getAttribute( 'data-rellax-zindex' ) || 0;
+      var dataMin = el.getAttribute( 'data-rellax-min' );
+      var dataMax = el.getAttribute( 'data-rellax-max' );
+      var dataMinX = el.getAttribute('data-rellax-min-x');
+      var dataMaxX = el.getAttribute('data-rellax-max-x');
+      var dataMinY = el.getAttribute('data-rellax-min-y');
+      var dataMaxY = el.getAttribute('data-rellax-max-y');
+      var mapBreakpoints;
+      var breakpoints = true;
+
+      if (!dataXsSpeed && !dataMobileSpeed && !dataTabletSpeed && !dataDesktopSpeed) {
+        breakpoints = false;
+      } else {
+        mapBreakpoints = {
+          'xs': dataXsSpeed,
+          'sm': dataMobileSpeed,
+          'md': dataTabletSpeed,
+          'lg': dataDesktopSpeed
+        };
+      }
 
       // initializing at scrollY = 0 (top of browser), scrollX = 0 (left of browser)
       // ensures elements are positioned based on HTML layout.
@@ -178,7 +263,7 @@ var rellax = createCommonjsModule(function (module) {
         wrapperPosY = scrollPosY - self.options.wrapper.offsetTop;
       }
       var posY = self.options.vertical ? ( dataPercentage || self.options.center ? wrapperPosY : 0 ) : 0;
-      var posX = self.options.horizontal ? ( dataPercentage || self.options.center ? (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft) : 0 ) : 0;
+      var posX = self.options.horizontal ? ( dataPercentage || self.options.center ? self.options.wrapper ? self.options.wrapper.scrollLeft : (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft) : 0 ) : 0;
 
       var blockTop = posY + el.getBoundingClientRect().top;
       var blockHeight = el.clientHeight || el.offsetHeight || el.scrollHeight;
@@ -192,9 +277,15 @@ var rellax = createCommonjsModule(function (module) {
       if(self.options.center){ percentageX = 0.5; percentageY = 0.5; }
 
       // Optional individual block speed as data attr, otherwise global speed
-      var speed = dataSpeed ? dataSpeed : self.options.speed;
+      var speed = (breakpoints && mapBreakpoints[currentBreakpoint] !== null) ? Number(mapBreakpoints[currentBreakpoint]) : (dataSpeed ? dataSpeed : self.options.speed);
+      var verticalSpeed = dataVerticalSpeed ? dataVerticalSpeed : self.options.verticalSpeed;
+      var horizontalSpeed = dataHorizontalSpeed ? dataHorizontalSpeed : self.options.horizontalSpeed;
 
-      var bases = updatePosition(percentageX, percentageY, speed);
+      // Optional individual block movement axis direction as data attr, otherwise gobal movement direction
+      var verticalScrollAxis = dataVericalScrollAxis ? dataVericalScrollAxis : self.options.verticalScrollAxis;
+      var horizontalScrollAxis = dataHorizontalScrollAxis ? dataHorizontalScrollAxis : self.options.horizontalScrollAxis;
+
+      var bases = updatePosition(percentageX, percentageY, speed, verticalSpeed, horizontalSpeed);
 
       // ~~Store non-translate3d transforms~~
       // Store inline styles and extract transforms
@@ -202,9 +293,10 @@ var rellax = createCommonjsModule(function (module) {
       var transform = '';
 
       // Check if there's an inline styled transform
-      if (style.indexOf('transform') >= 0) {
+      var searchResult = /transform\s*:/i.exec(style);
+      if (searchResult) {
         // Get the index of the transform
-        var index = style.indexOf('transform');
+        var index = searchResult.index;
 
         // Trim the style to the transform point and get the following semi-colon index
         var trimmedStyle = style.slice(index);
@@ -226,9 +318,19 @@ var rellax = createCommonjsModule(function (module) {
         height: blockHeight,
         width: blockWidth,
         speed: speed,
+        verticalSpeed: verticalSpeed,
+        horizontalSpeed: horizontalSpeed,
+        verticalScrollAxis: verticalScrollAxis,
+        horizontalScrollAxis: horizontalScrollAxis,
         style: style,
         transform: transform,
-        zindex: dataZindex
+        zindex: dataZindex,
+        min: dataMin,
+        max: dataMax,
+        minX: dataMinX,
+        maxX: dataMaxX,
+        minY: dataMinY,
+        maxY: dataMaxY
       };
     };
 
@@ -265,10 +367,10 @@ var rellax = createCommonjsModule(function (module) {
     // Ahh a pure function, gets new transform value
     // based on scrollPosition and speed
     // Allow for decimal pixel values
-    var updatePosition = function(percentageX, percentageY, speed) {
+    var updatePosition = function(percentageX, percentageY, speed, verticalSpeed, horizontalSpeed) {
       var result = {};
-      var valueX = (speed * (100 * (1 - percentageX)));
-      var valueY = (speed * (100 * (1 - percentageY)));
+      var valueX = ((horizontalSpeed ? horizontalSpeed : speed) * (100 * (1 - percentageX)));
+      var valueY = ((verticalSpeed ? verticalSpeed : speed) * (100 * (1 - percentageY)));
 
       result.x = self.options.round ? Math.round(valueX) : Math.round(valueX * 100) / 100;
       result.y = self.options.round ? Math.round(valueY) : Math.round(valueY * 100) / 100;
@@ -276,27 +378,97 @@ var rellax = createCommonjsModule(function (module) {
       return result;
     };
 
+    // Remove event listeners and loop again
+    var deferredUpdate = function() {
+      window.removeEventListener('resize', deferredUpdate);
+      window.removeEventListener('orientationchange', deferredUpdate);
+      (self.options.wrapper ? self.options.wrapper : window).removeEventListener('scroll', deferredUpdate);
+      (self.options.wrapper ? self.options.wrapper : document).removeEventListener('touchmove', deferredUpdate);
+
+      // loop again
+      loopId = loop(update);
+    };
+
     // Loop
     var update = function() {
       if (setPosition() && pause === false) {
         animate();
-      }
 
-      // loop again
-      loopId = loop(update);
+        // loop again
+        loopId = loop(update);
+      } else {
+        loopId = null;
+
+        // Don't animate until we get a position updating event
+        window.addEventListener('resize', deferredUpdate);
+        window.addEventListener('orientationchange', deferredUpdate);
+        (self.options.wrapper ? self.options.wrapper : window).addEventListener('scroll', deferredUpdate, supportsPassive ? { passive: true } : false);
+        (self.options.wrapper ? self.options.wrapper : document).addEventListener('touchmove', deferredUpdate, supportsPassive ? { passive: true } : false);
+      }
     };
 
     // Transform3d on parallax element
     var animate = function() {
       var positions;
       for (var i = 0; i < self.elems.length; i++){
-        var percentageY = ((posY - blocks[i].top + screenY) / (blocks[i].height + screenY));
-        var percentageX = ((posX - blocks[i].left + screenX) / (blocks[i].width + screenX));
+        // Determine relevant movement directions
+        var verticalScrollAxis = blocks[i].verticalScrollAxis.toLowerCase();
+        var horizontalScrollAxis = blocks[i].horizontalScrollAxis.toLowerCase();
+        var verticalScrollX = verticalScrollAxis.indexOf("x") != -1 ? posY : 0;
+        var verticalScrollY = verticalScrollAxis.indexOf("y") != -1 ? posY : 0;
+        var horizontalScrollX = horizontalScrollAxis.indexOf("x") != -1 ? posX : 0;
+        var horizontalScrollY = horizontalScrollAxis.indexOf("y") != -1 ? posX : 0;
+
+        var percentageY = ((verticalScrollY + horizontalScrollY - blocks[i].top + screenY) / (blocks[i].height + screenY));
+        var percentageX = ((verticalScrollX + horizontalScrollX - blocks[i].left + screenX) / (blocks[i].width + screenX));
 
         // Subtracting initialize value, so element stays in same spot as HTML
-        positions = updatePosition(percentageX, percentageY, blocks[i].speed);// - blocks[i].baseX;
+        positions = updatePosition(percentageX, percentageY, blocks[i].speed, blocks[i].verticalSpeed, blocks[i].horizontalSpeed);
         var positionY = positions.y - blocks[i].baseY;
         var positionX = positions.x - blocks[i].baseX;
+
+        // The next two "if" blocks go like this:
+        // Check if a limit is defined (first "min", then "max");
+        // Check if we need to change the Y or the X
+        // (Currently working only if just one of the axes is enabled)
+        // Then, check if the new position is inside the allowed limit
+        // If so, use new position. If not, set position to limit.
+
+        // Check if a min limit is defined
+        if (blocks[i].min !== null) {
+          if (self.options.vertical && !self.options.horizontal) {
+            positionY = positionY <= blocks[i].min ? blocks[i].min : positionY;
+          }
+          if (self.options.horizontal && !self.options.vertical) {
+            positionX = positionX <= blocks[i].min ? blocks[i].min : positionX;
+          }
+        }
+
+        // Check if directional min limits are defined
+        if (blocks[i].minY != null) {
+            positionY = positionY <= blocks[i].minY ? blocks[i].minY : positionY;
+        }
+        if (blocks[i].minX != null) {
+            positionX = positionX <= blocks[i].minX ? blocks[i].minX : positionX;
+        }
+
+        // Check if a max limit is defined
+        if (blocks[i].max !== null) {
+          if (self.options.vertical && !self.options.horizontal) {
+            positionY = positionY >= blocks[i].max ? blocks[i].max : positionY;
+          }
+          if (self.options.horizontal && !self.options.vertical) {
+            positionX = positionX >= blocks[i].max ? blocks[i].max : positionX;
+          }
+        }
+
+        // Check if directional max limits are defined
+        if (blocks[i].maxY != null) {
+            positionY = positionY >= blocks[i].maxY ? blocks[i].maxY : positionY;
+        }
+        if (blocks[i].maxX != null) {
+            positionX = positionX >= blocks[i].maxX ? blocks[i].maxX : positionX;
+        }
 
         var zindex = blocks[i].zindex;
 
@@ -327,9 +499,6 @@ var rellax = createCommonjsModule(function (module) {
     // Init
     init();
 
-    // Start the loop
-    update();
-
     // Allow to recalculate the initial values whenever we want
     self.refresh = init;
 
@@ -343,6 +512,9 @@ var _Vue; // tslint:disable-line variable-name
 var instanceMap = new WeakMap();
 var inserted = function (el, _a, vm) {
     var value = _a.value;
+    if (value === false) {
+        return;
+    }
     instanceMap.set(el, new rellax(el, value));
 };
 var destroy = function (el) {
@@ -358,6 +530,9 @@ var unbind = function (el) {
 var update = function (el, _a, vm) {
     var value = _a.value;
     destroy(el);
+    if (value === false) {
+        return;
+    }
     instanceMap.set(el, new rellax(el, value));
 };
 var install = function (InjectedVue) {
